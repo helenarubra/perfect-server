ce4782ba500fb4a4733864683a0f08a6a9029656
Johncore <gjohnes2018@gmail.com>
2021-04-09 19:56:57 -0300

    playerSaySpell Optimization

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -15747,7 +15747,7 @@ int LuaScriptInterface::luaSpellWords(lua_State* L)
 
 		if (lua_gettop(L) == 1) {
 			pushString(L, spell->getWords());
-			pushString(L, spell->getSeparator());
+			pushString(L, std::string(1, spell->getSeparator()));
 			return 2;
 		} else {
 			std::string sep = "";
@@ -15755,7 +15755,7 @@ int LuaScriptInterface::luaSpellWords(lua_State* L)
 				sep = getString(L, 3);
 			}
 			spell->setWords(getString(L, 2));
-			spell->setSeparator(sep);
+			spell->setSeparator((sep.empty() ? '"' : sep[0]));
 			pushBoolean(L, true);
 		}
 	} else {
@@ -16205,7 +16205,8 @@ int LuaScriptInterface::luaTalkactionSeparator(lua_State* L)
 	// talkAction:separator(sep)
 	TalkAction* talk = getUserdata<TalkAction>(L, 1);
 	if (talk) {
-		talk->setSeparator(getString(L, 2).c_str());
+		std::string sep = getString(L, 2);
+		talk->setSeparator((sep.empty() ? '"' : sep[0]));
 		pushBoolean(L, true);
 	} else {
 		lua_pushnil(L);
